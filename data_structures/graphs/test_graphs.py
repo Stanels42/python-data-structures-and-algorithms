8d9b3b6f4aa551f5ec4c7d7daf89564a02ec483e
Chris Stanley <CStanley4848@gmail.com>
2020-01-27 17:27:05 -0800

    Added the breath first traversal to the graph class

--- data_structures/graphs/test_graphs.py
+++ data_structures/graphs/test_graphs.py
@@ -68,3 +68,74 @@ def test_neighbors():
   g.add_edge(v1, v2, 5)
   assert g.get_neighbors(v1) == [(v2, 5)]
   assert g.get_neighbors(v2) == []
+
+
+def test_braeth_first_single():
+  """
+  Test the breath first with a single value
+  """
+  g = Graph()
+  v = g.add('v1')
+  lst = g.breath_first(v)
+
+  assert lst == ['v1']
+
+
+def test_empty():
+  """
+  The function needs a vertex and with out any raises an error.
+  """
+  g = Graph()
+  with pytest.raises(TypeError):
+    g.breath_first()
+
+
+def test_one_way():
+  """
+  Test the breath first can travels across the along edges
+  """
+  g = Graph()
+  v1 = g.add('v1')
+  v2 = g.add('v2')
+  g.add_edge(v1, v2)
+  lst_from_1 = g.breath_first(v1)
+  lst_from_2 = g.breath_first(v2)
+  assert lst_from_1 == ['v1', 'v2']
+  assert lst_from_2 == ['v2']
+
+
+def test_traverse_loop():
+  """
+  Test to see if the node are in a loop weather they get stuck looping over their selfs.
+  """
+  g = Graph()
+  v1 = g.add('v1')
+  v2 = g.add('v2')
+  v3 = g.add('v3')
+  g.add_edge(v1, v2)
+  g.add_edge(v2, v3)
+  g.add_edge(v3, v1)
+  lst_from_1 = g.breath_first(v1)
+  assert lst_from_1 == ['v1', 'v2', 'v3']
+
+
+def test_braeth_first():
+  g = Graph()
+  v1 = g.add('Pandora')
+  v2 = g.add('Arendelle')
+  v3 = g.add('Metroville')
+  v4 = g.add('Monstroplolis')
+  v5 = g.add('Narnia')
+  v6 = g.add('Naboo')
+
+  g.add_double_edge(v1, v2)
+  g.add_double_edge(v2, v3)
+  g.add_double_edge(v2, v4)
+  g.add_double_edge(v3, v5)
+  g.add_double_edge(v3, v6)
+  g.add_double_edge(v4, v5)
+  g.add_double_edge(v4, v6)
+  g.add_double_edge(v5, v6)
+  lst = g.breath_first(v1)
+
+  assert lst == ['Pandora', 'Arendelle', 'Metroville', 'Monstroplolis', 'Narnia', 'Naboo']
